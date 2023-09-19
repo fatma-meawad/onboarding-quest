f22c7b83bf41e46893fef534ae4b14f0bac3ba6f
Fatma <felsayedmeawad@gmail.com>
2023-09-19 09:02:20 +0000

    changed version_02 example

--- DisabilityAmount/version_02.py
+++ DisabilityAmount/version_02.py
@@ -1,7 +1,22 @@
-def calculate_disability_benefit(seniority: int, months_disabled: int, is_part_time: bool, age: int, disability_percentage: int, income: int, insurance_type: str):
-
-    if seniority < 2 or months_disabled > 12 or is_part_time or age < 62 or disability_percentage < 60 or income > 40000 or insurance_type != "disability":
-        return 0
+def disabilityAmount(seniority: int, monthsDisabled: int, isPartTime: bool, age: int, disabilityPercentage: int, income: int, insuranceType: str):
+    """Calculates the amount of disability benefits that an individual is eligible for.
+    Takes into account factors such as seniority, months disabled, whether the individual
+    is working part-time, age, disability percentage, income, and insurance type.
+    """
+    # Define the eligibility conditions
+    eligibility = (
+        seniority >= 2
+        and monthsDisabled <= 12
+        and not isPartTime
+        and age >= 62
+        and disabilityPercentage >= 60
+        and income <= 40000
+        and insuranceType == "disability"
+    )   
     
-    benefit_amount = (income * disability_percentage) / 100
-    return benefit_amount
+    # If eligible, calculate the benefit amount based on a formula
+    if eligibility:
+        benefitAmount = (income * disabilityPercentage) / 100
+        return benefitAmount
+
+    return 0
